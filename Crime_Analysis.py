2694f83b02fe85d5b96b21afb5fe22277339b0d3
salahbgm <116580792+salahbgm@users.noreply.github.com>
2023-10-27 00:05:44 +0200

    Update Crime_Analysis.py

--- Crime_Analysis.py
+++ Crime_Analysis.py
@@ -99,8 +99,7 @@ def display_unique_crime_types(df_gouv):
     st.write("Unique types of crime : ", unique_crime_types)
     st.write("Number of unique crime types : ", num_unique_crime_types)
 
-if st.checkbox('Show unique crime types'):
-    display_unique_crime_types(df_gouv)
+display_unique_crime_types(df_gouv)
 st.divider()
 
 
