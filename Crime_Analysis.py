724364c560368bfa62f03ebcb89af7a45a378a38
salahbgm <116580792+salahbgm@users.noreply.github.com>
2023-10-26 23:07:11 +0200

    Update Crime_Analysis.py

--- Crime_Analysis.py
+++ Crime_Analysis.py
@@ -500,7 +500,7 @@ if st.button('Predict and Plot'):
 
 
 end_time = time.time()
-#execution_time = end_time - start_time
+execution_time = end_time - start_time
 #display_time()
 #st.write(f"Temps de chargement de l'application : {execution_time:.2f} secondes")
 
